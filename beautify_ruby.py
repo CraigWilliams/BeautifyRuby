2ea0a09f724b72f6b166a7a69f2078b9f7b574ba
Craig Williams <cwilliams.allancraig@gmail.com>
2012-01-08 12:45:24 -0700

    Added check for document save to avoid possible data loss

--- beautify_ruby.py
+++ beautify_ruby.py
@@ -1,18 +1,19 @@
 import os.path
 from os import popen
-import sublime, sublime_plugin
+import sublime, sublime_plugin, sys
 
 class BeautifyRubyCommand(sublime_plugin.TextCommand):
   def run(self, edit):
     ext = os.path.basename(self.view.file_name())
 
     if ext.endswith(".rb"):
-      ruby_script  = self.ruby_script()
-      current_file = self.view.file_name()
-      args         = "/usr/bin/ruby '" + ruby_script + "' '" + unicode(current_file) + "'"
-      beautified   = os.popen(args).read()
+      if self.verify_document_saved():
+        ruby_script  = self.ruby_script()
+        current_file = self.view.file_name()
+        args         = "/usr/bin/ruby '" + ruby_script + "' '" + unicode(current_file) + "'"
+        beautified   = os.popen(args).read()
 
-      self.update_view(beautified)
+        self.update_view(beautified)
     else:
       sublime.error_message("This is not a Ruby file.")
 
@@ -25,3 +26,10 @@ class BeautifyRubyCommand(sublime_plugin.TextCommand):
     self.view.window().active_view().erase(edit, sublime.Region(0, self.view.window().active_view().size()))
     self.view.window().active_view().insert(edit, 0, contents)
     self.view.window().active_view().end_edit(edit)
+
+  def verify_document_saved(self):
+    if self.view.is_dirty():
+      sublime.error_message("To avoid possible data loss. Please save the document before running BeautifyRuby.")
+      return False
+    else:
+      return True
