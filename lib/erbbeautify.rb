e54f4239c6814ee230166b29cc8bdd6ded6b4958
Fire-Dragon-DoL <francesco.belladonna@gmail.com>
2013-01-12 11:03:40 +0100

    Added a fixme for erbbeautify

--- lib/erbbeautify.rb
+++ lib/erbbeautify.rb
@@ -1,6 +1,7 @@
 require 'rubygems'
 require 'htmlbeautifier'
 
+# FIXME: Erb file should handle line endings too, expecially the last "\n"
 def beautify(input, output)
   HtmlBeautifier::Beautifier.new(output).scan(input)
   output << "\n"
