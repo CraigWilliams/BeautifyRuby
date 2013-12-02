3db63aad10182262563463d215b3fcad605bd39b
zelig <viktor.tron@gmail.com>
2013-12-02 22:40:22 +0000

    major rewrite - prevents ST3 from installing package as archive (needed to execute scripts) - simplify code structure - better error handling - fixes bug with 2 buffers (when run_on_save and save_on_focus_lost set) - documentation improvements - put html_erb_patters in settings file explicitly

--- README.md
+++ README.md
@@ -1,19 +1,10 @@
 # BeautifyRuby
 
-## Known Issue
-When enabling `run_on_save` and `save_on_focus_lost`
-
-If you have two files open and one of them has unsaved changes,
-if you switch from the file with changes to the file without changes,
-the file's content without changes will be copied into the file
-that has changes.
-
-
 Beautifies Ruby code. This plugin uses the [Ruby Script Beautifier](http://www.arachnoid.com/ruby/rubyBeautifier.html) written by P.Lotus
 
 I made very little modification to get it to work with a Sublime Text 2 plugin.
 
-Erb html templates uses [Paul Battley's htmlbeautifier gem](https://github.com/threedaymonk/htmlbeautifier). This (as well as rubygems) is assumed to be installed as seen by the ruby interpreter.
+Erb html templates uses [Paul Battley's htmlbeautifier gem](https://github.com/threedaymonk/htmlbeautifier). This (as well as rubygems) is assumed to be installed as seen by the ruby interpreter. Note that if you beautify and erb file but `htmlbeautifier` is not found, the error message is 'check your ruby interpreter settings', do not be misled.
 
 This package offers a pre-save hook, i.e., your ruby and erb files will be reformatted automatically before saving. To activate this feature, set:
 
@@ -26,17 +17,11 @@ The sublime command "beautify_ruby" performs a save after formatting. You can di
 You can change the file patterns handled by this plugin in the settings:
 
     "file_patterns": [ "\\.html\\.erb", "\\.rb", "\\.rake", "Rakefile", "Gemfile" ],
+    "html_erb_patterns": ["\\.html\\.erb"],
 
+This plugin uses ruby scripts to beautify your buffer, so it needs ruby installed. You can configure your ruby interpreter under Preferences -> Package Settings -> BeautifyRuby -> Settings Default/User. Although the default should work on linux and osx, not setting this right is a common problem.
 
-### Key Binding
-
-```
-  ctrl + cmd + k on OS X, or ctrl + alt + k on Windows
-```
-
-If your file turns blank you can configure your ruby interpreter under Preferences -> Package Settings -> BeautifyRuby -> Settings Default
-
-I do
+If you do not use the system ruby, type in your favourite shell:
 
 ```
   which ruby
@@ -55,6 +40,12 @@ If you use project-specific rubies and gem sets managed with `rvm`, then simply 
       "ruby": "~/.rvm/bin/rvm-auto-ruby",
 
 and then the `htmlbeautifier` gem is found even if it is only installed for this project.
+
+### Key Binding
+
+```
+  ctrl + cmd + k on OS X, or ctrl + alt + k on Windows
+```
 
 # Installation
 
--- beautify_ruby.py
+++ beautify_ruby.py
@@ -2,16 +2,8 @@ import os.path
 import sublime, sublime_plugin, sys, re
 import subprocess
 
-class PerformEditCommand(sublime_plugin.TextCommand):
-  def run(self, edit, data):
-    edit = edit
-    size = self.view.size()
-    region = sublime.Region(0, size)
-    self.view.replace(edit, region, data)
-
-  def is_enabled(self):
-        return True
-
+# PLUGIN_FOLDER = os.path.dirname(os.path.realpath(__file__))
+PLUGIN_FOLDER = os.path.join(sublime.packages_path(), 'BeautifyRuby')
 
 class BeautifyRubyOnSave(sublime_plugin.EventListener):
   def on_pre_save(self, view):
@@ -21,83 +13,88 @@ class BeautifyRubyOnSave(sublime_plugin.EventListener):
 
 class BeautifyRubyCommand(sublime_plugin.TextCommand):
   def run(self, edit, error=True, save=True):
-    self.settings = sublime.load_settings('BeautifyRuby.sublime-settings')
-    if self.is_ruby_file():
-      save = save and self.settings.get('save_on_beautify')
-      self.get_selection_position()
-      self.active_view = self.view.window().active_view()
-      self.buffer_region = sublime.Region(0, self.active_view.size())
-      self.view.run_command("perform_edit", { "data":self.beautify_buffer() } )
+    self.load_settings()
+    self.filename = self.view.file_name()
+    self.fname = os.path.basename(self.filename)
+    self.erb = self.is_erb_file()
+    try:
+      if self.erb or self.is_ruby_file():
+        self.beautify_buffer(edit)
+        if save and self.settings.get('save_on_beautify'):
+          self.view.run_command('save')
+      else:
+        if error:
+          raise Exception("This is not a Ruby file.")
+    except:
+      msg = "Error: {0}".format(sys.exc_info()[1])
+      sublime.error_message(msg)
 
-      if save:
-        self.view.run_command('save')
-      self.reset_selection_position()
-    else:
-      if error:
-        sublime.error_message("This is not a Ruby file.")
+  def beautify_buffer(self, edit):
+    buffer_region = sublime.Region(0, self.view.size())
+    buffer_text = self.view.substr(buffer_region)
+    if buffer_text == "":
+      return
+    self.save_viewport_state()
+    beautified_buffer = self.pipe(self.cmd(), buffer_text)
+    self.check_valid_output(beautified_buffer)
+    self.view.replace(edit, buffer_region, beautified_buffer)
+    self.reset_viewport_state()
 
-  def beautify_file(self):
-    save_document_if_dirty(self)
-    subprocess.Popen(self.cmd(self.filename))
-
-  def beautify_buffer(self):
-    working_dir = os.path.dirname(self.filename)
-    body = self.active_view.substr(self.buffer_region)
-    beautifier = subprocess.Popen(self.cmd(), shell=True, cwd=working_dir, stdin=subprocess.PIPE, stdout=subprocess.PIPE)
-    out = beautifier.communicate(body.encode("utf-8"))[0].decode('utf8')
-    out_array = out.splitlines()
-    out_final = "\n".join(out_array)
-    if (out == "" and body != ""):
-      sublime.error_message("check your ruby interpreter settings")
-      return body
-    else:
-      return out_final
-
-  def reset_selection_position(self):
-    if sublime.version() < '3':
-      self.view.sel().clear()
-      self.view.sel().add(self.region)
-      self.view.show_at_center(self.region)
-
-  def get_selection_position(self):
-    sel         = self.view.sel()[0].begin()
-    pos         = self.view.rowcol(sel)
-    target      = self.view.text_point(pos[0], 0)
-    self.region = sublime.Region(target)
-
-  def save_document_if_dirty(self):
-    if self.view.is_dirty():
-      self.view.run_command('save')
+  def check_valid_output(self, text):
+    if text == "":
+      msg = "invalid output. Check your ruby interpreter settings"
+      raise Exception(msg)
 
   def cmd(self, path = "-"):
-    ruby_interpreter = self.settings.get('ruby') or "/usr/bin/env ruby"
-    if self.is_erb_file():
+    if self.erb:
       script_name = 'erbbeautify.rb'
     else:
       script_name = 'rbeautify.rb'
-    ruby_script  = os.path.join(sublime.packages_path(), 'BeautifyRuby', 'lib', script_name)
+    ruby_interpreter = self.settings.get('ruby') or "/usr/bin/env ruby"
+    ruby_script  = os.path.join(PLUGIN_FOLDER, 'lib', script_name)
+    if not os.path.exists(ruby_script):
+      msg = "script: '" + ruby_script + "' not found."
+      raise Exception(msg)
     args = ["'" + str(path) + "'"]
     if self.settings.get('tab_or_space') != "space":
       args.insert(0, '-t')
-    command = ruby_interpreter + " '" + ruby_script + "' " + ' '.join(args)
-    return command
+    return ruby_interpreter + " '" + ruby_script + "' " + ' '.join(args)
 
-  def is_erb_file(self):
-    file_patterns = self.settings.get('html_erb_patterns') or ["\.html\.erb"]
-    patterns = re.compile(r'\b(?:%s)\b' % '|'.join(file_patterns))
-    if patterns.search(self.fname):
-      return True
-    else:
-      return False
+  def finalize_output(self, text):
+    lines = text.splitlines()
+    finalized_output = "\n".join(lines)
+    if self.view.settings().get("ensure_newline_at_eof_on_save") and not text.endswith("\n"):
+      text += "\n"
+    return finalized_output
+
+  def load_settings(self):
+    self.settings = sublime.load_settings('BeautifyRuby.sublime-settings')
+
+  def save_viewport_state(self):
+    self.previous_selection = [(region.a, region.b) for region in self.view.sel()]
+    self.previous_position = self.view.viewport_position()
+
+  def reset_viewport_state(self):
+    self.view.set_viewport_position((0, 0,), False)
+    self.view.set_viewport_position(self.previous_position, False)
+    self.view.sel().clear()
+    for a, b in self.previous_selection:
+      self.view.sel().add(sublime.Region(a, b))
 
   def is_ruby_file(self):
-    self.filename = self.view.window().active_view().file_name()
-    self.fname         = os.path.basename(self.filename)
-    file_patterns = self.settings.get('file_patterns') or ['.rb', '.rake']
-    if self.settings.get('html_erb_patterns'):
-      file_patterns  = file_patterns + self.settings.get('html_erb_patterns')
+    file_patterns = self.settings.get('file_patterns') or ['\.rb', '\.rake']
+    return self.match_pattern(file_patterns)
+
+  def is_erb_file(self):
+    file_patterns = self.settings.get('html_erb_patterns') or ['\.html\.erb']
+    return self.match_pattern(file_patterns)
+
+  def match_pattern(self, file_patterns):
     patterns = re.compile(r'\b(?:%s)\b' % '|'.join(file_patterns))
-    if patterns.search(self.fname):
-      return True
-    else:
-      return False
+    return patterns.search(self.fname)
+
+  def pipe(self, cmd, text):
+    cwd = os.path.dirname(self.filename)
+    beautifier = subprocess.Popen(cmd, shell=True, cwd=cwd, stdin=subprocess.PIPE, stdout=subprocess.PIPE)
+    out = beautifier.communicate(text.encode("utf-8"))[0]
+    return out.decode('utf8')
