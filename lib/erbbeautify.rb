e26bc78fa817843edbf7a9977c731c2201aa4ef5
Alexis Ramis <zerocool4u2@gmail.com>
2015-11-08 23:34:55 -0300

    tab_size and translate_spaces in html.erb files
    support for tab_size and translate_tabs_to_spaces options in html.erb files
    with htmlbeatufier, translate_spaces_to_tabs is on a pending pull request from
    htmlbeautifier, but it will not be a problem have it implemented already.
    
    https://github.com/threedaymonk/htmlbeautifier/pull/32
    
    tab_size will be ignored if translate_tabs_to_spaces is false
    
    Changes:
    	modificado: lib/erbbeautify.rb

--- lib/erbbeautify.rb
+++ lib/erbbeautify.rb
@@ -1,7 +1,37 @@
 require 'htmlbeautifier'
+module ERBeautify
 
-def beautify(input, output)
-  output.write(HtmlBeautifier.beautify(input))
+  class << self
+    def beautify(input, output, options = {})
+      output.write(HtmlBeautifier.beautify(input, options))
+    end
+
+    def main
+      if(!ARGV[0])
+        STDERR.puts "usage: Ruby filenames or \"-\" for stdin."
+        exit 0
+      else
+        path = ARGV.shift
+        config = generate_config(ARGV)
+        options = Hash.new
+        #https://github.com/threedaymonk/htmlbeautifier/pull/32 waiting on pull request, if is accepted  translate_tabs_to_spaces
+        #will start to work
+        translate_spaces_to_tabs = config['translate_tabs_to_spaces'] == 'False' ? { translate_spaces_to_tabs: true } : Hash.new
+        tab_size = config['tab_size'].to_i != 0 ? { tab_stops: config['tab_size'].to_i } : Hash.new
+        options = options.merge(tab_size).merge(translate_spaces_to_tabs)
+        beautify $stdin.read.force_encoding('utf-8'), $stdout, options
+      end
+    end
+
+    def generate_config args
+      args.each_slice(2).with_object({}) do |parameter, result|
+        result[parameter.first.gsub('--','').gsub('-','_')] = parameter.last
+      end
+    end
+  end
 end
 
-beautify $stdin.read.force_encoding('utf-8'), $stdout
+# if launched as a standalone program, not loaded as a module
+if __FILE__ == $0
+  ERBeautify.main
+end
